88a5c69c41fdadfd67e92c27069eb9f2aac532c7
nura <nashrahili22@twq.idserve.net>
2023-01-05 09:55:41 +0300

    Initial Commit

---
+++ ofra./ofra_App.swift
@@ -0,0 +1,17 @@
+//
+//  ofra_App.swift
+//  ofra.
+//
+//  Created by nura on 12/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ofra_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
